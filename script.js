e7009c907b0c1129bcac0ef85639a83d8158d493
Marcos Antonio <98793728+devmarcosantonio@users.noreply.github.com>
2025-08-08 18:58:03 -0300

    nova versao

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
-window.addEventListener('scroll', function() {
+const sections = ['home', 'about', 'skills', 'contact'];
+const menuItem = document.querySelectorAll('.menu-list__item');
+const imagemProgramador = document.querySelector('#programador-img');
+const skillsItem = document.querySelectorAll('.skills__item');
+
+window.addEventListener('scroll', function () {
   const menu = document.querySelector('.menu');
   const home = document.querySelector('#home');
-  if(window.scrollY > 10) {
+  const metadeTela = window.innerHeight / 2;
+
+  if (window.scrollY > 2) {
     menu.classList.add('menu--scrolled');
     home.classList.add('home--scrolled');
   } else {
     menu.classList.remove('menu--scrolled');
     home.classList.remove('home--scrolled');
   }
+  
+  sections.forEach((id, index) => {
+    const section = document.getElementById(id);
+    const item = menuItem[index];
+    if (section && item) {
+      const topo = section.getBoundingClientRect().top;
+      if (topo <= metadeTela && topo + section.offsetHeight > metadeTela) {
+        menuItem.forEach(i => i.classList.remove('item__ativo'));
+        item.classList.add('item__ativo');
+      }
+    }
+  });
+});
+
+skillsItem.forEach(item => {
+  item.addEventListener('mouseover', () => {
+    item.querySelector('.skills__item__texto').style.fontSize = '20px'
+  });
+  item.addEventListener('mouseout', () => {
+    item.querySelector('.skills__item__texto').style.fontSize = '0'
+  });
 });
